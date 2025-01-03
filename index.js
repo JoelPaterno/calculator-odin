a28c6fa11b3cf2bdce2eda9cebd27b915bd5b73b
joelpaterno <joelpaterno1@gmail.com>
2025-01-04 10:41:33 +1100

    feat: add operate function

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ function divide(first, second) {
     return first / second;
 };
 
+function operate(operator, first, second) {
+    let result = 0;
+    switch (operator) {
+        case "+":
+            result = add(first, second);
+        case "-":
+            result = subtract(first, second);
+        case "*":
+            result = multiply(first, second);
+        case "/":
+            result = divide(first, second);
+        default:
+            result = 0;
+    };
+    return result;
+};
